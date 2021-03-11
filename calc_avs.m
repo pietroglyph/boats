e6d68950e73706ab3521be5f4a507cf1b2547981
Bill Fan <fanbenlong@gmail.com>
2021-03-11 15:17:10 -0500

    AVS: Fixed index offset issue

--- calc_avs.m
+++ calc_avs.m
@@ -6,27 +6,31 @@ function [avs, phis, moment_arms]= calc_avs(W, D, L, f_contour, f_rho)
         moment_arms(phi) = moment_arm_func(phi, W, D, L, f_contour, f_rho);
     end
     
+    crop_min = 10;
+    crop_max = 170;
     moment_arms_cropped = moment_arms(10:170);
     
     % Get the AVS
     % The AVS satisfies two conditions: 
     % 1. The sign changes. 
     % 2. The derivative is negative
-    avs = phis(find(diff(sign(moment_arms_cropped)) ...
-                & diff(moment_arms_cropped) < 0)); 
+    found_indices = crop_min - 1 + ...
+            find(diff(sign(moment_arms_cropped)) &...
+                 diff(moment_arms_cropped) < 0);
+
+    v_avs = phis(found_indices); 
     
-    if isempty(avs)
+    if isempty(v_avs)
         avs = 0;
         return
     end
     
     % Sometimes the first element of phis is mistakenly included
-    if avs(1) == phis(1)
-        avs = avs(2:end);
+    if v_avs(1) == phis(1)
+        v_avs = v_avs(2:end);
     end
     
-    if length(avs) > 1
-        error("multiple AVS's found with params [%f, %f]", W, D)
-    end
+    % Return the first (minimal) avs
+    avs = v_avs(1);
     
 end
